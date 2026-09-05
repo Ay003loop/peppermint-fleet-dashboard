6d6b3b8973fd847fb030ad0f690bbef41754a8a1
Ayan1 / projects <ayan.kashyap003@gmail.com>
2026-09-06 00:53:55 +0530

    Fix variable name from 'ttp' to 'http'

--- simulator.js
+++ simulator.js
@@ -1,4 +1,4 @@
-  const ttp = require('http');
+  const http = require('http');
   const port = process.env.PORT || 3000;
 http.createServer((req,res)=>{
   res.writeHead(200,{'Content-Type' : 'text/plain'});
